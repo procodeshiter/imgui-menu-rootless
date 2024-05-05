1746ccc49783e533589f19c41a6746ddc0266e8e
dulces suenos <146747623+procodeshiter@users.noreply.github.com>
2024-05-05 22:22:56 +0300

    standoff 2 menu rootless
    https://t.me/brutaltrip

---
+++ imgui menu standoff 2/ImGuiDrawView.mm
@@ -0,0 +1,246 @@
+//Require standard library
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import <Foundation/Foundation.h>
+//Imgui library
+#import "Esp/CaptainHook.h"
+#import "Esp/ImGuiDrawView.h"
+#import "IMGUI/imgui.h"
+#import "IMGUI/imgui_impl_metal.h"
+#import "IMGUI/Honkai.h"
+//Patch library
+#import "5Toubun/NakanoIchika.h"
+#import "5Toubun/NakanoNino.h"
+#import "5Toubun/NakanoMiku.h"
+#import "5Toubun/NakanoYotsuba.h"
+#import "5Toubun/NakanoItsuki.h"
+#import "5Toubun/dobby.h"
+
+#define kWidth  [UIScreen mainScreen].bounds.size.width
+#define kHeight [UIScreen mainScreen].bounds.size.height
+#define kScale [UIScreen mainScreen].scale
+
+@interface ImGuiDrawView () <MTKViewDelegate>
+@property (nonatomic, strong) id <MTLDevice> device;
+@property (nonatomic, strong) id <MTLCommandQueue> commandQueue;
+@end
+
+@implementation ImGuiDrawView
+
+//I usually let the function for hooking in here...
+void (*huy)(void *instance);
+void _huy(void *instance)
+{
+huy(instance);
+}
+
+static bool MenDeal = true;
+
+
+- (instancetype)initWithNibName:(nullable NSString *)nibNameOrNil bundle:(nullable NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+
+    _device = MTLCreateSystemDefaultDevice();
+    _commandQueue = [_device newCommandQueue];
+
+    if (!self.device) abort();
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGui::StyleColorsClassic();
+    
+    ImFont* font = io.Fonts->AddFontFromMemoryCompressedTTF((void*)Honkai_compressed_data, Honkai_compressed_size, 45.0f, NULL, io.Fonts->GetGlyphRangesDefault());
+    
+    ImGui_ImplMetal_Init(_device);
+
+    return self;
+}
+
++ (void)showChange:(BOOL)open
+{
+    MenDeal = open;
+}
+
+- (MTKView *)mtkView
+{
+    return (MTKView *)self.view;
+}
+
+- (void)loadView
+{
+
+ 
+
+    CGFloat w = [UIApplication sharedApplication].windows[0].rootViewController.view.frame.size.width;
+    CGFloat h = [UIApplication sharedApplication].windows[0].rootViewController.view.frame.size.height;
+    self.view = [[MTKView alloc] initWithFrame:CGRectMake(0, 0, w, h)];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.mtkView.device = self.device;
+    self.mtkView.delegate = self;
+    self.mtkView.clearColor = MTLClearColorMake(0, 0, 0, 0);
+    self.mtkView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
+    self.mtkView.clipsToBounds = YES;
+
+}
+
+
+
+#pragma mark - Interaction
+
+- (void)updateIOWithTouchEvent:(UIEvent *)event
+{
+    UITouch *anyTouch = event.allTouches.anyObject;
+    CGPoint touchLocation = [anyTouch locationInView:self.view];
+    ImGuiIO &io = ImGui::GetIO();
+    io.MousePos = ImVec2(touchLocation.x, touchLocation.y);
+
+    BOOL hasActiveTouch = NO;
+    for (UITouch *touch in event.allTouches)
+    {
+        if (touch.phase != UITouchPhaseEnded && touch.phase != UITouchPhaseCancelled)
+        {
+            hasActiveTouch = YES;
+            break;
+        }
+    }
+    io.MouseDown[0] = hasActiveTouch;
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self updateIOWithTouchEvent:event];
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self updateIOWithTouchEvent:event];
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self updateIOWithTouchEvent:event];
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self updateIOWithTouchEvent:event];
+}
+
+#pragma mark - MTKViewDelegate
+
+- (void)drawInMTKView:(MTKView*)view
+{
+   
+    
+    ImGuiIO& io = ImGui::GetIO();
+    io.DisplaySize.x = view.bounds.size.width;
+    io.DisplaySize.y = view.bounds.size.height;
+
+    CGFloat framebufferScale = view.window.screen.scale ?: UIScreen.mainScreen.scale;
+    io.DisplayFramebufferScale = ImVec2(framebufferScale, framebufferScale);
+    io.DeltaTime = 1 / float(view.preferredFramesPerSecond ?: 120);
+    
+    id<MTLCommandBuffer> commandBuffer = [self.commandQueue commandBuffer];
+    
+
+//Define your bool/function in here
+    static bool show_s0 = false;    
+    static bool show_s1 = false;    
+    static bool show_s2 = false;    
+    static bool show_s3 = false;    
+    static bool show_s4 = false;    
+    static bool show_s5 = false;    
+    static bool show_s6 = false;                    
+    static bool show_s7 = false;        
+    static bool show_s8 = false;      
+    static bool show_s9 = false;     
+    static bool show_s10 = false;     
+    static bool show_s11 = false;     
+    static bool show_s12 = false;     
+
+//Define active function
+    static bool show_s0_active = false;
+    
+        
+        if (MenDeal == true) {
+            [self.view setUserInteractionEnabled:YES];
+        } else if (MenDeal == false) {
+            [self.view setUserInteractionEnabled:NO];
+        }
+
+        MTLRenderPassDescriptor* renderPassDescriptor = view.currentRenderPassDescriptor;
+        if (renderPassDescriptor != nil)
+        {
+            id <MTLRenderCommandEncoder> renderEncoder = [commandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
+            [renderEncoder pushDebugGroup:@"ImGui Jane"];
+
+            ImGui_ImplMetal_NewFrame(renderPassDescriptor);
+            ImGui::NewFrame();
+            
+            ImFont* font = ImGui::GetFont();
+            font->Scale = 15.f / font->FontSize;
+            
+            CGFloat x = (([UIApplication sharedApplication].windows[0].rootViewController.view.frame.size.width) - 360) / 2;
+            CGFloat y = (([UIApplication sharedApplication].windows[0].rootViewController.view.frame.size.height) - 300) / 2;
+            
+            ImGui::SetNextWindowPos(ImVec2(x, y), ImGuiCond_FirstUseEver);
+            ImGui::SetNextWindowSize(ImVec2(400, 300), ImGuiCond_FirstUseEver);
+            
+            if (MenDeal == true)
+            {                
+   ImGui::SetNextWindowSize({263.f,154.f});
+
+ImGui::Begin("you name menu");
+
+ImGui::Checkbox("Overlay", &show_s0);
+
+ImGui::Checkbox("ESP", &show_s1);
+
+ImGui::Checkbox("ESP line", &show_s2);
+
+ImGui::Checkbox("ESP hp", &show_s3);
+
+ if (ImGui::Button("Status")) {  
+                ImGui::SameLine();
+ 
+                
+            }
+            ImDrawList* draw_list = ImGui::GetBackgroundDrawList();
+
+
+        
+//Hook function example
+    static dispatch_once_t onceToken;
+            dispatch_once(&onceToken, ^{
+                //use DobbyHook, same kind of MSHookFunction but working on JIT, Dopamine!
+                DobbyHook((void *)(getRealOffset(ENCRYPTOFFSET("0x5F145F8"))), (void *)_huy, (void **)&huy);
+            });
+
+
+            ImGui::Render();
+            ImDrawData* draw_data = ImGui::GetDrawData();
+            ImGui_ImplMetal_RenderDrawData(draw_data, commandBuffer, renderEncoder);
+          
+            [renderEncoder popDebugGroup];
+            [renderEncoder endEncoding];
+
+            [commandBuffer presentDrawable:view.currentDrawable];
+        }
+
+        [commandBuffer commit];
+}
+
+- (void)mtkView:(MTKView*)view drawableSizeWillChange:(CGSize)size
+{
+    
+}
+
+@end
+
